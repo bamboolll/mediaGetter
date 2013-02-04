7317ec2eba4c20c9bf5bb08722e062175325d891
heckarim <heckarim@localhost.localdomain>
2013-02-04 15:52:55 +0700

    Script for downloading Zing album

---
+++ zingD.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+url=$1
+album=$2
+echo "url is $url"
+echo "output folder: $album"
+out1="album.html"
+out2="lines"
+out3="lines.links"
+
+#Step 1: Download HTML 
+wget $url -O $out1
+#Step 2: Parse for link line
+cat $out1  | sed 's/<\/a>/\n/g' | grep music-download > $out2
+#Step 3: Parse for links and names and put to file
+rm -rf $out3
+while read line 
+do
+	link=$( echo $line | sed 's/<a href="\([^"]*\)".*/\1/g' );
+	name=$( echo $line | sed 's/.*title="Download \([^"]*\)".*/\1/g' );
+	echo "link-${link}:name-${name}" >>$out3;
+done < $out2;
+#Step 4: Download file
+rm -rf ${album};
+mkdir ${album};
+while read line
+do
+	link=$( echo $line | sed 's/link-\(.*\):name-.*/\1/g' );
+	name=$( echo $line | sed 's/.*:name-\(.*\)/\1/g' ); 
+	#fix space in name
+	name=$( echo $name | sed 's/ /./g' );
+	echo "Downloading ${name} ....";
+	wget ${link} -O "${album}/${name}.mp3";
+done < $out3;
+
+
+
+
+
